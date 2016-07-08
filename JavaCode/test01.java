68b1b283f96d0a274eba2be23754b57f6821aeab
endless3cross3 <endless3cross3@gmail.com>
2016-07-08 22:43:16 +0800

    測試 StringBuilder

--- JavaCode/test01.java
+++ JavaCode/test01.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-class test01{
+class test01 {
 	public static void main(String[] args) {
 		ArrayList<String> aList = new ArrayList<String>();
 		aList.add("book");
@@ -8,5 +8,14 @@ class test01{
 		int x = (int) y;
 		System.out.println(x);
 		System.out.println(aList.contains("book"));
+
+		StringBuilder builder = new StringBuilder();
+		builder.append("測試");
+		builder.append("字串");
+		builder.insert(1, "ABC");
+		// builder.delete(4, 5);
+		// builder.reverse();
+		System.out.println(builder.toString());
+		System.out.println(builder);
 	}
 }
